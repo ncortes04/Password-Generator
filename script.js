7a7ae7a4dcb0d2a0b86794e6f9173c9dc75221a4
ncortes04 <nickapoleon@gmail.com>
2022-08-29 18:28:34 -0700

    created java script password generator

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+const password1 = document.getElementById('#password')
+stringamt = (9)
+numamt = (5)
+specialamt = (8) 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function passstring() {
+  var length = (stringamt);
+  var string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+  var password = "";
+  var character = "";
+while(password.length<length) {
+    entity1 = Math.ceil(string.length * Math.random() * Math.random())
+    hold = string.charAt( entity1 );
+    character += hold;
+    password = character;
+ }
+ return password;
 }
+console.log(passstring())
 
+function passnum() {
+    var length = (numamt);
+    var number = '0123456789';
+    var password = "";
+    var character = "";
+  while(password.length<length) {
+      entity2 = Math.ceil(number.length * Math.random() * Math.random())
+      hold = number.charAt( entity2 );
+      character += hold;
+      password = character;
+   }
+   return password;
+  }
+  console.log(passnum())
+  function passspecial() {
+    var length = (specialamt);
+    var special = "!@#$%^&*\|[]{};'.<>/";
+    var password = "";
+    var character = "";
+  while(password.length<length) {
+      entity3 = Math.ceil(special.length * Math.random() * Math.random())
+      hold = special.charAt( entity3 );
+      character += hold;
+      password = character;
+   }
+   password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+   return password;
+  }
+console.log(passspecial())
+var password = (passnum() + passspecial() + passstring())
+password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
+console.log(password)
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", passnum);
