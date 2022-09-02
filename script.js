41cc7e8d394593f159ff2c161ed17698ffd9607f
ncortes04 <nickapoleon@gmail.com>
2022-09-01 22:05:48 -0700

    changed code to be more concise and simple  including random umber generator. Changed arrays to include decimal values rather than typing letters and symbols. Added checkboxes and value sliders. Linked HTML and js get elementbyID. Added comments explaining the function of the code and functions.

--- script.js
+++ script.js
@@ -1,56 +1,66 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-const password1 = document.getElementById('#password')
-stringamt = (9)
-numamt = (5)
-specialamt = (8) 
-// Write password to the #password input
-function passstring() {
-  var length = (stringamt);
-  var string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
-  var password = "";
-  var character = "";
-while(password.length<length) {
-    entity1 = Math.ceil(string.length * Math.random() * Math.random())
-    hold = string.charAt( entity1 );
-    character += hold;
-    password = character;
- }
- return password;
-}
-console.log(passstring())
+var characterAmountRange = document.getElementById('passAmountSlider')
+var characterAmountNumber = document.getElementById('passAmountNumber')
+var includeUppercaseElement = document.getElementById('includeUppercase')
+var includeNumbersElement = document.getElementById('includeNumbers')
+var includeSymbolsElement = document.getElementById('includeSymbols')
+var form = document.getElementById('passwordGeneratorForm')
+var endpass = document.getElementById('endpass')
 
-function passnum() {
-    var length = (numamt);
-    var number = '0123456789';
-    var password = "";
-    var character = "";
-  while(password.length<length) {
-      entity2 = Math.ceil(number.length * Math.random() * Math.random())
-      hold = number.charAt( entity2 );
-      character += hold;
-      password = character;
-   }
-   return password;
+/* This submit buttons calls and changes the value every time the button is pressed. As you can see these variables check if the check boxes are selected and what the value of character amount is. These variables then called in the password variable that has a function that randomly sorts that characters within the numbers, letters, and symbols. */
+form.addEventListener('submit', function(event){
+  event.preventDefault()
+  var characterAmount = characterAmountNumber.value
+  var includeUppercase = includeUppercaseElement.checked
+  var includeNumbers = includeNumbersElement.checked
+  var includeSymbols = includeSymbolsElement.checked
+  var password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  endpass.innerText = password
+})
+/* These varibles are vey intersting yet crucial for my password generator.I originally typed out each letter, number, and symbol but found an easier method of using the ASCII character chart and called the characters by thier deciaml value*/
+/* The uppercaseCodes variable created an array that grabs the letters 65-90 which is A-Z including all the 24 letters in between*/
+var uppercaseCodes = arrayFromLowToHigh(65, 90)
+/* The lowercaseCodes value creates an array that grabs the letters 97-122 which is a-z in lowercase; including all the 24 letters in between*/
+var lowercaseCodes = arrayFromLowToHigh(97, 122)
+/* The numberCodes value creates an array that grabs the numbers 48-57 which is just 0-9*/
+var numberCodes = arrayFromLowToHigh(48, 57)
+/* The symbolCodes value creates an array that grabs the symbols (33-47), (58-64), (91-96), and (123-126) which is various different symbols within the ASCII chart and all combined with .concat*/
+var symbolCodes = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+/* these are add event listeners for inputs that */
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+/* the generate oaswird variable sets the default password to be lowercase. The iff statments say if the  Include Uppercase or Include Numbers orInclude Symbols ar checked combine them (.concat) with the lowercase code. If multiple are pressed then combine them too
+*/
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  var charCodes = lowercaseCodes
+  if (includeUppercase) charCodes = charCodes.concat(uppercaseCodes)
+  if (includeSymbols) charCodes = charCodes.concat(symbolCodes)
+  if (includeNumbers) charCodes = charCodes.concat(numberCodes)
+  /* variable paswordCharacters runs a for loop that grabs the previous charCodes variable and randomly assorts then within the selected value criteria*/
+  var passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
   }
-  console.log(passnum())
-  function passspecial() {
-    var length = (specialamt);
-    var special = "!@#$%^&*\|[]{};'.<>/";
-    var password = "";
-    var character = "";
-  while(password.length<length) {
-      entity3 = Math.ceil(special.length * Math.random() * Math.random())
-      hold = special.charAt( entity3 );
-      character += hold;
-      password = character;
-   }
-   password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-   return password;
+  return passwordCharacters.join('')
+}
+/* This function is called above that sepcifies the characters within are an array and should be sorted from low to high */
+function arrayFromLowToHigh(low, high) {
+  var array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
   }
-console.log(passspecial())
-var password = (passnum() + passspecial() + passstring())
-password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-console.log(password)
-// Add event listener to generate button
-generateBtn.addEventListener("click", passnum);
+  return array
+}
+/* This functions grabs the users target value for the character slider and is being called in thecharacter ammount number as well as range */
+function syncCharacterAmount(event) {
+  var value = event.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
